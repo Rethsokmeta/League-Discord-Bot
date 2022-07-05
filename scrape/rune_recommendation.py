f5f82e331968e7ee5a351a4591a9b9f25a34ec41
Sokmeta-Reth <metasokreth@gmail.com>
2022-07-05 13:59:04 +0700

    fix crashed chromedriver

--- scrape/rune_recommendation.py
+++ scrape/rune_recommendation.py
@@ -10,7 +10,9 @@ from io import BytesIO
 class League:
     options = Options()
     options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    options.headless = True
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=options, executable_path=os.environ.get("CHROMEDRIVER_PATH"))
 
